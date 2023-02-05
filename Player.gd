41c224ad0d10cb9a5e19ef9dd1c6336ea5c7d31c
Joni Räsänen <joni.rasanen@iki.fi>
2023-02-05 11:53:22 +0200

    Assets and animations, vegetables are in progress

--- Player.gd
+++ Player.gd
@@ -46,14 +46,24 @@ func _physics_process(delta):
         if Input.is_action_just_pressed("ui_up") or Input.is_action_pressed("ui_up"):
             velocity.y = -jump_strength*encumbrance_modifier.y
     else:
+        $AnimatedSprite.animation = "Jump"
         if Input.is_action_just_released("ui_up") and velocity.y < -small_jump*encumbrance_modifier.y:
             velocity.y = -small_jump*encumbrance_modifier.y
+            $AnimatedSprite.animation = "Idle"
     
     if input.x == 0:
         velocity.x = move_toward(velocity.x, 0, friction)
+        if is_on_floor():
+            $AnimatedSprite.animation = "Idle"
     else:
         velocity.x = move_toward(velocity.x, encumbrance_modifier.x*speed*input.x, acceleration)
-    
+        if is_on_floor():
+            $AnimatedSprite.animation = "Run"
+
+    if input.x < 0:
+        $AnimatedSprite.flip_h = true
+    elif input.x > 0:
+        $AnimatedSprite.flip_h = false
     velocity = move_and_slide(velocity, Vector2.UP)
     
     if position.x < level_x_min:
@@ -65,13 +75,14 @@ func pick_vegetable():
     var tile_position : Vector2 = convert_to_tilemap(position) + Vector2(-0.5, 1.0)
     tile_position = Vector2(floor(tile_position.x), floor(tile_position.y))
     
-    print(get_node(vegetable_tiles).get_cellv(tile_position))
+    var can_submit_to_basket : bool = get_node(vegetable_tiles).get_cellv(tile_position) == 1 \
+        or get_node(vegetable_tiles).get_cellv(tile_position + Vector2(1.0,0.0)) == 1
     
-    if get_node(vegetable_tiles).get_cellv(tile_position) == TileMap.INVALID_CELL:
-        pass
-    elif get_node(vegetable_tiles).get_cellv(tile_position) == 1:
+    if can_submit_to_basket:
         print("Adding vegetables to basket")
         get_node(basket).transfer_contents($"Resource Holder")
+    elif get_node(vegetable_tiles).get_cellv(tile_position) == TileMap.INVALID_CELL:
+        pass
     else:
         print("Stealing vegetable")
         get_node(vegetable_tiles).set_cellv(tile_position, TileMap.INVALID_CELL)
